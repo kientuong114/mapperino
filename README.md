89f9d9e428c41a6ba40cf5fbea0e305e7ec6969b
Kien Tuong Truong <kientuong@gmail.com>
2021-03-07 15:56:51 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Mapperino
+
+Small tool to slice an image in smaller squares and upload each square to an online service such as imgur or Amazon S3.
+
+The main objective of this tool is to provide a way for users to create big images in Minecraft while using the [Image2Map](https://github.com/TheEssem/Image2Map) mod for Fabric.
+
+## Setup and Usage
+
+```
+usage: main.py [-h] [-c COUNT] [-t TILESIZE] [-o OUTPUT]
+               [--no-upload]
+               {imgur,s3} image
+
+Split an image, upload it to imgur and automatically write the
+commands in game to create maps
+
+positional arguments:
+  {imgur,s3}            Service to use to upload images
+  image                 The image to split
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c COUNT, --count COUNT
+                        Number of splits
+  -t TILESIZE, --tilesize TILESIZE
+                        Size of the tiles in px
+  -o OUTPUT, --output OUTPUT
+                        Output the commands on the file instead of
+                        stdout
+  --no-upload           Prevents upload of images, for testing
+```
+
+The tool allows to specify either the number of horizontal splits or the size of the split. The splits will all be squares, which means that the vertical split cannot be specified indipendently.
+
+The user must either create an [imgur](https://imgur.com/) or an [Amazon AWS](https://aws.amazon.com) account.
+
+### Imgur
+
+For imgur, the client ID should be assigned to a `IMGUR_CLIENT_ID` environment variable.
+
+Then, use the command with `imgur` as a service, for example:
+
+```
+python main.py imgur my_image.png -c 3
+```
+
+### S3
+
+For Amazon AWS S3, you should install the AWS CLI tool and run `aws configure` (if you haven't previously done so).
+
+Afterwards, just run the command with `s3` as a service, for example:
+
+```
+python main.py s3 my_image.png -c 3
+```
---
+++ main.py
@@ -0,0 +1,88 @@
+import argparse
+import itertools
+import os
+import tempfile
+from PIL import Image
+
+IMGUR_CLIENT_ID = os.environ.get('IMGUR_CLIENT_ID', None)
+S3_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME', 'mapperino')
+transfer = None
+
+parser = argparse.ArgumentParser(description='Split an image, upload it to imgur and automatically write the commands in game to create maps')
+parser.add_argument('service', choices=['imgur', 's3'], help='Service to use to upload images')
+parser.add_argument('image', help='The image to split')
+parser.add_argument('-c', '--count', help='Number of splits', type=int)
+parser.add_argument('-t', '--tilesize', help='Size of the tiles in px', type=int)
+parser.add_argument('-o', '--output', help='Output the commands on the file instead of stdout')
+parser.add_argument('--no-upload', help='Prevents upload of images, for testing', action='store_true')
+
+def split_image(img_name: str, h_splits: int):
+    img = Image.open(img_name)
+    w, h = img.size
+    tile_size = w // h_splits
+    grid = list(itertools.product(range(0, w, tile_size), range(0, h, tile_size)))
+    imgs = []
+    for i, j in grid:
+        box = (i, j, i+tile_size, j+tile_size)
+        imgs.append(img.crop(box))
+    return imgs
+
+def split_image_by_tile_size(img_name: str, tile_size):
+    img = Image.open(img_name)
+    w, h = img.size
+    grid = list(itertools.product(range(0, w, tile_size), range(0, h, tile_size)))
+    imgs = []
+    print(len(grid))
+    for i, j in grid:
+        box = (i, j, i+tile_size, j+tile_size)
+        imgs.append(img.crop(box))
+    return imgs
+
+def upload_image(client, file, filename):
+    global transfer
+    if args.service == 'imgur':
+        uploaded = client.upload_image(file, title=filename)
+        return uploaded.link
+    elif args.service == 's3':
+        if not transfer:
+            transfer = S3Transfer(client)
+        transfer.upload_file(file, S3_BUCKET_NAME, filename, extra_args={'ACL': 'public-read'})
+        return f'{client.meta.endpoint_url}/{S3_BUCKET_NAME}/{filename}'
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    if not (args.count or args.tilesize):
+        parser.error('You must specify either the tile count or the tile size')
+    if args.count and args.tilesize:
+        parser.error('You must specify only one between tile count and tile size')
+    if args.count:
+        imgs = split_image(args.image, args.count)
+    elif args.tilesize:
+        imgs = split_image_by_tile_size(args.image, args.tilesize)
+
+    if args.service == 'imgur':
+        import pyimgur
+        client = pyimgur.Imgur(IMGUR_CLIENT_ID)
+    elif args.service == 's3':
+        import boto3
+        from boto3.s3.transfer import S3Transfer
+        client = boto3.client('s3', region_name='eu-central-1')
+
+    image_name = args.image.replace(' ', '_')
+
+    if not args.no_upload:
+        commands = []
+
+        with tempfile.TemporaryDirectory() as tempdir:
+            for i, img in enumerate(imgs):
+                filename = os.path.join(tempdir, str(i) + '_' + image_name)
+                img.save(filename)
+                uploaded = upload_image(client, filename, 'MapperinoTool_' + str(i) + '_' + image_name)
+                commands.append(f'/mapcreate {uploaded}')
+
+        if args.output:
+            with open(args.output) as f:
+                f.write('\n'.join(commands))
+        else:
+            print('\n'.join(commands))
